4a821636c41ab438cc5d6ee3d9b940528737586e
Robert Böhnke <robert.boehnke@gmail.com>
2011-07-09 21:38:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# A embed.ly client for Jekyll
+
+This is a handy embed.ly client for people that use the static-site generator
+jekyll.
+
+## How to install
+
+1. Make sure you have the _json_ and _domainatrix_ gems installed.
+2. Download the `embedly.rb` file and place it in the `_plugins/` inside your
+   Jekyll project directory.
+3. Go to the embed.ly site, register an account and get your API key.
+4. Edit your `_config.yml` as described below.
+5. Make use of the new `embedly`-Liquid tag somewhere on your site.  
+   E.g. `{% embedly  http://soundcloud.com/mightyoaksmusic/rainier %}`
+6. Compile your site.
+
+Please not that github-pages does not allow the use of plugins, if you want
+to make use of this plugin, you have to compile your site yourself.
+
+## How to set up the `_config.yml`
+
+First, pass in your newly acquired API key like so:
+
+    embedly:
+      api_key: abcdefg123456780cafebabe101cat44
+
+You can further customize your embeds adding host-specific parameters.
+
+    embedly:
+      api_key: abcdefg123456780cafebabe101cat44
+
+      soundcloud:
+        color: 0066FF # SoundCloud specific paramter for colorful players
+        width: 500px
+
+      vimeo:
+        width: 500px
+
+For a list of supported parameters, please have a look at
+[embed.ly’s documentation][docs] as well as the documentation for the oEmbed
+implementation of the specific hosts.
+
+Provider specific parameters are currently not working properly across the
+board. Please let me know what works and what doesn't.
+
+## Style your embeds
+
+Your embed will be wrapped inside a `div`-tag that has classes matching the
+embeds type, provider as well as the generic `embed`.
+
+E.g.
+
+    `{% embedly  http://soundcloud.com/mightyoaksmusic/rainier %}`
+    
+will result in
+
+    <div class="embed rich soundcloud">
+      ...
+    </div>
+
+[docs]: http://embed.ly/docs/endpoints/arguments
---
+++ embedly.rb
@@ -0,0 +1,75 @@
+require 'rubygems'
+require 'net/https'
+require 'uri'
+require 'json'
+require 'domainatrix'
+
+module Jekyll
+  class Embedly < Liquid::Tag
+    @@EMBEDLY_PARAMETERS = ['maxwidth', 'maxheight', 'format', 'callback',
+                            'wmode', 'allowscripts', 'nostyle', 'autoplay',
+                            'videosrc', 'words', 'chars', 'width', 'height']
+    
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text.strip
+    end
+
+    def render(context)
+      @config  = context.registers[:site].config['embedly']
+      @api_key = @config['api_key']
+      
+      if @api_key.nil?
+        raise "You must provide embed.ly api key."
+      end
+
+      embed @text
+    end
+
+    private
+
+    def embed(url)
+      provider = Domainatrix.parse(url).domain
+
+      parameters = ""
+      if @config[provider]
+        @config[provider].each do |key, value|
+          if @@EMBEDLY_PARAMETERS.member? key
+            parameters << "&#{key}=#{value}"
+          else
+            url << (url.match(/\?/) ? "&" : "?") << "#{key}=#{value}"
+          end
+        end
+      end
+
+      encoded_url = CGI::escape url
+      embedly_url = URI.parse "http://api.embed.ly/1/oembed?key=#{@api_key}" +
+                              "&url=#{encoded_url}#{parameters}"
+      
+      json_rep = JSON.parse resolve(embedly_url)
+
+      compose json_rep
+    end
+    
+    def compose(json_rep)
+      html     = json_rep['html']
+      type     = json_rep['type']
+      provider = json_rep['provider_name'].downcase
+      
+      "<div class=\"embed #{type} #{provider}\">#{html}</div>"
+    end
+    
+    def resolve(uri)
+      response = Net::HTTP.get_response(uri)
+
+      unless response['location'].nil? and response['Location'].nil?
+        resolve URI.parse(response['location']) or
+                URI.parse(response['Location'])
+      else
+        response.body
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('embedly', Jekyll::Embedly)
